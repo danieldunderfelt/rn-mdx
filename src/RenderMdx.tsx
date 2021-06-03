4f4ee1683227e261c7c17ed2646287085b4dbd92
Daniel Dunderfelt <daniel@developsuperpowers.com>
2021-06-03 19:30:37 +0300

    Custom components through 'components' prop.

--- src/RenderMdx.tsx
+++ src/RenderMdx.tsx
@@ -1,20 +1,40 @@
-import React, { useMemo } from 'react'
-import components from './markdownComponents'
+import React, { ComponentType, useMemo } from 'react'
+import markdownComponents from './markdownComponents'
 import MDX from '@mdx-js/runtime'
-import { styles } from './style/styles'
+import { MarkdownStyles, styles } from './style/styles'
 
 const defaultScope = {}
 
-export function RenderMdx({ children, scope = {}, componentStyle = {} }) {
-  const defaultComponents = useMemo(() => components(styles(componentStyle)), [componentStyle])
+type RenderMdxProps = {
+  children?: string
+  scope?: Record<string, unknown>
+  components?: Record<string, ComponentType>
+  componentStyle?: MarkdownStyles
+}
+
+export function RenderMdx({
+  children,
+  components = {},
+  scope = {},
+  componentStyle = {},
+}: RenderMdxProps) {
+  const defaultComponents = useMemo(
+    () => markdownComponents(styles(componentStyle)),
+    [componentStyle]
+  )
   const contentScope = { ...defaultScope, ...scope }
+
+  const mdxComponents = useMemo(
+    () => ({ ...defaultComponents, ...components }),
+    [components, defaultComponents]
+  )
 
   if (!children) {
     return null
   }
 
   return (
-    <MDX components={defaultComponents} scope={contentScope}>
+    <MDX components={mdxComponents} scope={contentScope}>
       {children}
     </MDX>
   )
